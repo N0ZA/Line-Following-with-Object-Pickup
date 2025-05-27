be4cde5a3e81c2e3cacb648767ce7a80b8ace161
N0ZA <mohamedmaahir@gmail.com>
2025-05-27 15:41:52 +0400

    added a second gripper

--- esp32.ino
+++ esp32.ino
@@ -1,6 +1,7 @@
 #include <ESP32Servo.h>
 
-Servo gripperServo;
+Servo gripperServo1;
+Servo gripperServo2;
 
 void setup() {
   Serial.begin(115200);  // ESP32 typically uses 115200 baud rate
@@ -11,9 +12,12 @@ void setup() {
   ESP32PWM::allocateTimer(2);
   ESP32PWM::allocateTimer(3);
   
-  gripperServo.attach(18);  // GPIO 18 (good PWM pin for ESP32)
-  gripperServo.write(35);   // Start in closed position
+  gripperServo1.attach(18);  // GPIO 18 (good PWM pin for ESP32)
+  gripperServo1.write(35);   // Start in closed position
   
+  gripperServo2.attach(19);  // GPIO 19 (good PWM pin for ESP32)
+  gripperServo2.write(95);   // Start in closed position
+
   Serial.println("ESP32 Servo Gripper Control");
   Serial.println("Type 'J' to open, 'K' to close the gripper.");
 }
@@ -23,11 +27,19 @@ void loop() {
     char command = Serial.read();
     
     if (command == 'J' || command == 'j') {
-      gripperServo.write(35);     // Open gripper
+      gripperServo1.write(35);     // Open gripper
       Serial.println("Gripper opened.");
     } 
     else if (command == 'K' || command == 'k') {
-      gripperServo.write(90);    // Close gripper
+      gripperServo1.write(90);    // Close gripper
+      Serial.println("Gripper closed.");
+    }
+    else if (command == 'B' || command == 'B') {
+      gripperServo2.write(110);    // Close gripper
+      Serial.println("Gripper closed.");
+    }
+    else if (command == 'V' || command == 'v') {
+      gripperServo2.write(95);    // Close gripper
       Serial.println("Gripper closed.");
     }
   }
